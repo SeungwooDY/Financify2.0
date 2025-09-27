0a5a85d30eeaddbf0b0ac8ef464cda4acb102548
SeungwooDY <sdyoon0701@gmail.com>
2025-09-27 13:17:36 -0400

    all frontend tabs working

--- financify/src/components/transactions/TransactionTable.tsx
+++ financify/src/components/transactions/TransactionTable.tsx
@@ -9,7 +9,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
+} from "@/components/ui/Table"
 import { Button } from "@/components/ui/button"
 import { Badge } from "@/components/ui/badge"
 import { 
