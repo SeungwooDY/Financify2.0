67f232afb3d89e78788f11695786bfbffafddac6
SeungwooDY <sdyoon0701@gmail.com>
2025-09-26 22:22:17 -0400

    servers branch

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,30 @@
 import uvicorn
-import fastapi
-import pydantic
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import os
+import shutil
+import pytesseract
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.orm import declarative_base, sessionmaker
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List, Optional, Annotated
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+###
+### Post requests here
+###
